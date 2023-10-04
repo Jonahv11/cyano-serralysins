bb8bd82f9d89bd65c8e1753908d0dca3c703c741
blasks <blasks@uw.edu>
2023-10-03 18:32:13 -0700

    finished script to pull out cycog genes

--- results/2-phylogenies/get-cycog-proteins.sh
+++ results/2-phylogenies/get-cycog-proteins.sh
@@ -22,9 +22,9 @@ REFS=../../data/genomes/
 if [[ ! -d data/ ]]; then
     mkdir data
 fi
-# pull out entries for each cycog group into its own csv
+# pull out gene for each cycog group into its own file
 for cycog in 60001830 60001883 60001888 60002365; do
-    grep ${cycog} ${CYCOGS} >> data/${cycog}.csv
+    grep ${cycog} ${CYCOGS} | cut -d , -f 4 >> data/${cycog}.txt
 done
 
 # 2. Concatenate all reference sequences
@@ -32,13 +32,21 @@ for genome in `tail -n +2 $CYCOGS | cut -d , -f 6 | sort | uniq`; do
     cat ${REFS}/${genome}/${genome}.genes.fna >> data/sequences.fna
 done
 
-# # 3. Pull out genes of each cycog group into unique fasta files
-# for cycog in 60001830 60001883 60001888 60002365; do
-#     echo ${cycog}
-#     for geneid in `cut -d , -f 4 data/${cycog}.csv`; do
-#         echo ${geneid}
-#         # need to figure out how this awk command works
-#         # awk 'BEGIN{RS=">"} /2717395913*/ && NR>1 { print ">" $0 }' data/sequences.fna 
-#         # https://stackoverflow.com/questions/49731386/extract-sequences-from-multifasta-file-by-id-in-file-using-awk
-#     done
-# done
+# 3. Pull out genes of each cycog group into unique fasta files
+for cycog in 60001830 60001883 60001888 60002365; do
+    printf "Finding genes in CyCOG: ${cycog}\n"
+    # reads the list of ids into an array, then compares the first field
+    # of each record in the sequence files against the id list and 
+    # prints out the matches
+    awk 'NR==FNR{ids[$0]; next} ($1 in ids) { printf ">" $0 }' data/${cycog}.txt RS='>' data/sequences.fna >> data/${cycog}.genes.fna
+    # anther slower solutino that loops over the sequence file over 
+    # and over for each id being searched
+    # for geneid in `cut -d , -f 4 data/${cycog}.csv`; do
+    #     printf "\tsearching ${geneid}...\n"
+    #     awk -v id="${geneid}" -v RS='>' '$1 == id {print RS $0}' data/sequences.fna >> data/${cycog}.genes.fna
+    # done
+done
+
+# 4. clean up
+rm data/*.txt
+rm data/sequences.fna
